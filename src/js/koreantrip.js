fda7f4e8355c498a28a25012df4452ebe99c2039
DESKTOP-5GGCUIJ\park <soomin8248@gmail.com>
2021-07-12 21:29:58 +0900

    국내여행 메인페이지 수정

--- src/js/koreantrip.js
+++ src/js/koreantrip.js
@@ -1,6 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {
     // visual 이미지 fadeIn/fadeOut 효과주기(4초간격)
     let visualItems = document.querySelector('.visual_items');
+    let jeju_cons = document.querySelectorAll('.jeju_con');
+    let items = document.querySelectorAll('.item');
+
     let koreatripvisu = new Swiper('.visual-swiper', {
         effect : 'fade',
         speed : 500,
@@ -46,29 +49,69 @@ document.addEventListener("DOMContentLoaded", function () {
     // fadeStart();
 
     //제주여행 메뉴 클릭시 해당 아이템들 나타나도록 하기
-    const jejuItems = document.querySelectorAll('.jeju_items');
-    const jejuMenu = document.querySelectorAll('.jeju_menu li');
+    jeju_cons.forEach(jeju_con => {
+        jeju_con.addEventListener("click",function(){
+            let viewIdx = this.getAttribute('subSeq');
+            items.forEach(function(thsIdx,index){
+                if (index == viewIdx) {
+                    thsIdx.style.display = 'flex';
+                } else {
+                    thsIdx.style.display = 'none';
+                }
+            });
+            jeju_cons.forEach(function(thsIdx) {
+                if(thsIdx.getAttribute('subSeq') == viewIdx){
+                    thsIdx.classList.add('on');
+                } else {
+                    thsIdx.classList.remove('on');
+                }
+            });
+        });
+    });
+    
+    // const jejuItems = document.querySelectorAll('.jeju_items');
+    // const jejuMenu = document.querySelectorAll('.jeju_menu li');
 
-    jejuMenu.forEach((item, index) => {
-        item.addEventListener('click', e => {
-            jejuMenu.forEach(item => item.classList.remove('on'));
-            e.target.classList.add('on');
-            jejuItems.forEach(item => item.classList.remove('on'));
-            jejuItems[index].classList.add('on');
-        })
-    })
+    // jejuMenu.forEach((item, index) => {
+    //     item.addEventListener('click', e => {
+    //         jejuMenu.forEach(item => item.classList.remove('on'));
+    //         e.target.classList.add('on');
+    //         jejuItems.forEach(item => item.classList.remove('on'));
+    //         jejuItems[index].classList.add('on');
+    //     })
+    // })
 
     //MD추천 아이템들 무한슬라이드 효과주기(버튼, 스크롤)
-    var swiper = new Swiper('.korMD-swiper', {
+    new Swiper('.korMD-swiper', {
         slidesPerView: 4,
-        spaceBetween: 30,
-        loop: true,
+        spaceBetween: 24,
+        loop : true,
         navigation: {
-            nextEl: '.korMD-swiper-right',
-            prevEl: '.korMD-swiper-left',
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
         },
         mousewheel: {
             invert: true,
         },
+        keyboard: {
+            enabled: true,
+            onlyInViewport: false,
+        },
+        autoplay: {
+            delay: 4000,
+        },
     });
+    
+    // var swiper = new Swiper('.korMD-swiper', {
+    //     slidesPerView: 4,
+    //     spaceBetween: 30,
+    //     loop: true,
+    //     navigation: {
+    //         nextEl: '.korMD-swiper-right',
+    //         prevEl: '.korMD-swiper-left',
+    //     },
+    //     mousewheel: {
+    //         invert: true,
+    //     },
+    // });
 })
