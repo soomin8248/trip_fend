f2871c4137ef07bb952a862e36303e725249c10a
wlstjjw <momo37025@naver.com>
2021-07-15 11:53:17 +0900

    freelist swipe 작업중(destroy?)

--- src/js/freelist.js
+++ src/js/freelist.js
@@ -1,14 +1,40 @@
 document.addEventListener("DOMContentLoaded", function () {
     const bigmenu = document.querySelectorAll('.tour_bigmenu ul li');
     const tourtype = document.querySelectorAll('.tour_type');
-    bigmenu.forEach(function (item, index) {
+    const tourslide = document.querySelectorAll('.show_tourslide');
+    // var swiper;
+
+    bigmenu.forEach((item, index) => {
         item.addEventListener('click', function () {
             bigmenu.forEach(item => item.classList.remove('on'));
             item.classList.add('on');
+
             tourtype.forEach(item => item.classList.remove('on'));
             tourtype[index].classList.add('on');
-        })
-    })
+
+            // swiper.destroy(true, true);
+            let classon = tourslide[index];
+            slideOn(classon);
+        });
+    });
+
+
+    function slideOn(classon) {
+        new Swiper(classon, {
+            slidesPerView: 2,
+            spaceBetween: 20,
+            loop: true,
+            mousewheel: {
+                invert: true,
+            },
+            autoplay: {
+                delay: 3000,
+                disableOnInteraction: false,
+            },
+        });
+    }
+    // slideOn(tourslide[0]);
+
 
     const smallmenu = document.querySelectorAll('.tour_smallmenu li');
     const tour_lists = document.querySelectorAll('.tour_lists');
@@ -21,37 +47,4 @@ document.addEventListener("DOMContentLoaded", function () {
             tour_lists[index].classList.add('on');
         })
     })
-
-    var swiper = new Swiper('.free-swiper', {
-        slidesPerView: 2,
-        spaceBetween: 20,
-        loop: true,
-        navigation: {
-            nextEl: '.free-swiper-right',
-            prevEl: '.free-swiper-left',
-        },
-        mousewheel: {
-            invert: true,
-        },
-        autoplay: {
-            delay: 3000,
-            disableOnInteraction: false,
-        },
-    });
-    var swiper = new Swiper('.package-swiper', {
-        slidesPerView: 2,
-        spaceBetween: 20,
-        loop: true,
-        navigation: {
-            nextEl: '.package-swiper-right',
-            prevEl: '.package-swiper-left',
-        },
-        mousewheel: {
-            invert: true,
-        },
-        autoplay: {
-            delay: 3000,
-            disableOnInteraction: false,
-        },
-    });
 })
