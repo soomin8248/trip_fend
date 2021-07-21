d628882a6d8a8cbb5a33f348aac8edcb8185e4e9
jinyoung <cood6482@naver.com>
2021-07-21 21:05:47 +0900

    specialtrip scss, js 작업 완료

--- src/js/specialtrip.js
+++ src/js/specialtrip.js
@@ -9,3 +9,40 @@ document.addEventListener("DOMContentLoaded", function () {
         },
     });
 });
+
+// 탭 이동
+const tabBtns = document.querySelectorAll('#tab_btn > li');
+const itemCon = document.querySelectorAll('.item_contents');
+
+tabBtns.forEach(function(item, index){
+    item.addEventListener('click', function(){
+        tabBtns.forEach(item => item.classList.remove('on'));
+        item.classList.add('on');
+        itemCon.forEach(item => item.classList.remove('on'));
+        itemCon[index].classList.add('on');
+
+    })
+})
+
+// 전체탭에서 더보기 버튼 클릭 했을 때 높이 늘어나기
+const moreBtn = document.querySelector('.morebtn');
+const allItem = document.querySelector('#all');
+moreBtn.addEventListener('click', function(){
+    allItem.classList.add('view');
+})
+
+// topbtn 클릭하면 상단으로 이동하기
+const topBtn = document.querySelector('#topbtn');
+
+topBtn.addEventListener('click', function () {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+})
+// topbtn 스크롤하면 나타나기
+addEventListener('scroll', function () {
+    let sct = document.documentElement.scrollTop;
+    if (sct > 100) {
+        topBtn.classList.add('on');
+    } else {
+        topBtn.classList.remove('on');
+    }
+})
